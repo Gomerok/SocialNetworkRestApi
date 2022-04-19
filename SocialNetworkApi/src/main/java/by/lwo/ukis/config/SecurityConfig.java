4127d02981b1f62cace2c689eaf557e32553e2df
Gomerok <danila.gomerov@gmail.com>
2022-04-19 18:04:38 +0300

    add swagger

--- SocialNetworkApi/src/main/java/by/lwo/ukis/config/SecurityConfig.java
+++ SocialNetworkApi/src/main/java/by/lwo/ukis/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -54,4 +55,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui.html/**", "/swagger-ui/**", "/v3/api-docs/**");
+         }
 }
